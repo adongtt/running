c9c59e16913248119a358e08b3816553f92d0e98
adongtt <240732222@qq.com>
2024-01-04 21:47:34 +0800

    Initial commit

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,29 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Running Page',
+  siteUrl: 'https://yihong.run',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Blog',
+      url: 'https://baidu.com',
+    },
+    {
+      name: 'About',
+      url: 'https://baidu.com',
+    },
+  ],
+};
+
+export default data;
