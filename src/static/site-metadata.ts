df14b7eb965c491e836ba32ab8176b76a7e3cd0d
adongtt <240732222@qq.com>
2024-01-05 10:40:44 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,19 +10,19 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: 'adong's Running Page',
+  siteUrl: 'https://adongtt.github.io/running/',
+  logo: 'https://www.custombaseballgloves.com/static/images/adong.jpg',
   description: 'Personal site and blog',
   navLinks: [
-    {
-      name: 'Blog',
-      url: 'https://baidu.com',
-    },
-    {
-      name: 'About',
-      url: 'https://baidu.com',
-    },
+    // {
+    //   name: 'Blog',
+    //   url: 'https://adongtt.github.io/running/',
+    // },
+    // {
+    //   name: 'About',
+    //   url: 'https://adongtt.github.io/running/',
+    // },
   ],
 };
 
