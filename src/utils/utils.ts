51dd1cf33e1a927d928c1733cac2e5773109c2af
adongtt <240732222@qq.com>
2024-01-14 22:56:09 +0800

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -236,7 +236,7 @@ const getBoundsForGeoData = (
   }).fitBounds(cornersLongLat, { padding: 200 });
   let { longitude, latitude, zoom } = viewState;
   if (features.length > 1) {
-    zoom = 11.5;
+    zoom = 8;
   }
   return { longitude, latitude, zoom };
 };
