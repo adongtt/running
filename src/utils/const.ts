d65e7b0baa68f036c4afa1f63bb368bd8cd65166
adongtt <240732222@qq.com>
2025-02-21 14:53:31 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -32,7 +32,7 @@ const USE_DASH_LINE = false;
 // styling: route line opacity: [0, 1]
 const LINE_OPACITY = 0.4;
 // styling: map height
-const MAP_HEIGHT = 600;
+const MAP_HEIGHT = 1200;
 const PRIVACY_MODE = true;
 //set to `false` if you want to hide the road label characters
 const ROAD_LABEL_DISPLAY = false;
